83ac8dee6de801e072a4c242cde9aa964f8becd1
Katya Sheps <katya@sheps.ru>
2019-06-30 13:40:10 +0300

    Override onCreate, onStart, onResume, onPause, onStop, onDestroy, onRestart Add login

--- app/src/main/java/ru/sheps/android/homework621/MainActivity.java
+++ app/src/main/java/ru/sheps/android/homework621/MainActivity.java
@@ -3,6 +3,8 @@ package ru.sheps.android.homework621;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +12,8 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    int activityNumber = 0;
+    private static final String TAG = "Lifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         Button btnNext = findViewById(R.id.buttonNext);
         TextView textView = findViewById(R.id.name);
         Random random = new Random();
-        textView.setText("http://myfile.org/" + random.nextInt(100));
+        activityNumber = random.nextInt(100);
+        textView.setText("http://myfile.org/" + activityNumber);
 
     }
 
@@ -33,5 +38,36 @@ public class MainActivity extends AppCompatActivity {
     public void onClickBack(View view) {
         finish();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, activityNumber + " onStart: " + this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, activityNumber + " onResume: " + this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, activityNumber + " onPause: " + this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, activityNumber + " onStop: " + this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, activityNumber + " onDestroy: " + this);
+    }
+
 }
 
